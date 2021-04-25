739b76c75705bfce571fdc6696038917e237111e
GENIUS <naikmahesh.naik@gmail.com>
2021-04-25 19:58:15 +0530

    Add Given user input replace the word

---
+++ Replace.py
@@ -0,0 +1,12 @@
+"""given user input replace the word """
+"""using length function calculate length"""
+try:
+    username = str(input("Enter Name: "))
+    length = len(username)
+    if length >= 3:
+       print(f"hello {username}, How are you?")
+    else:
+        print("username should be minimum 3 character")
+
+except Exception:
+        print("Exception occured")
