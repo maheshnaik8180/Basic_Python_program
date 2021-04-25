7aa651515cfd1b286a747c1d4890681254f514ca
GENIUS <naikmahesh.naik@gmail.com>
2021-04-25 19:26:08 +0530

    Add harmonic_number problem

---
+++ harmonicNumber.py
@@ -0,0 +1,11 @@
+
+try:
+
+    number = int(input("Enter the value of nth number : "))
+    s = 0.0
+    for num in range(1, number + 1):
+        s = s + 1 / num
+    print("Sum is: ", round(s, 3))
+
+except Exception:
+    print("Exception occurred")
