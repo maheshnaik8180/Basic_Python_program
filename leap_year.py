60b806557689a0ab11591eecd2e5cd17cfa8df7b
GENIUS <naikmahesh.naik@gmail.com>
2021-04-25 20:12:13 +0530

    Add check leap year or not given user input

---
+++ leap_year.py
@@ -0,0 +1,18 @@
+#Check if leap year or not
+
+year=int(input("Enter a year: "))
+
+minyear=1000
+maxyear=9999
+
+"""check the user input minimum and maximum limit"""
+
+if year >= minyear and year <= maxyear :
+        if year % 4 == 0 and year % 100 != 0:
+            print ("Entered year is a Leap Year ")
+        elif year % 400 == 0:
+            print("Entered year is a Leap Year ")
+        else:
+            print ("Entered year is not a Leap Year ")
+else:
+    print ("Invalid Year, Please Enter 4 digit valid year ")
