2b750fc524c307af5cfaba5fb98a12c23027a7b1
GENIUS <naikmahesh.naik@gmail.com>
2021-04-27 15:52:28 +0530

    Add : Add Stopwatch Problem Using user input

--- logical/stopwatch.py
+++ logical/stopwatch.py
@@ -4,12 +4,14 @@ modified_date = '24/04/2021'
 author = 'Mahesh Naik'
 description = Stop watch problem"""
 import time
+
 """define function for stopwatch format"""
 def stopwatch(sec):
     min = (sec // 3600)//60
     sec = ((sec % 3600)%60)
     hours = min//3600
     print('%02d:%02d:%02d' %(hours, min, sec))
+
 time_start=input("Press key to start the time")
 Start = time.time()
 
